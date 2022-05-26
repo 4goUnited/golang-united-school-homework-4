ab82ad6c642c5ef5bfd327c9cd0d9571f73dbf80
4goUnited <4goUnited@gmail.com>
2022-05-26 23:39:03 +0500

    Fifth solution

--- string_sum.go
+++ string_sum.go
@@ -6,7 +6,6 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
-	"unicode"
 )
 
 //use these errors as appropriate, wrapping them with fmt.Errorf function
@@ -15,8 +14,6 @@ var (
 	errorEmptyInput = errors.New("input is empty")
 	// Use when the expression has number of operands not equal to two
 	errorNotTwoOperands = errors.New("expecting two operands, but received more or less")
-	// Use when the expression has invalid input characters
-	errorInvalidInput = errors.New("There is not valid input characters in your expression")
 )
 
 // Implement a function that computes the sum of two int numbers written as a string
@@ -29,34 +26,28 @@ var (
 //
 // Use the errors defined above as described, again wrapping into fmt.Errorf
 
-func StringSum(input string) (output string, err error) {
+func StringSum(input string) (string, error) {
 
 	input = strings.TrimSpace(input)
 	if input == "" {
-		return "", fmt.Errorf("Nothing to calculate.\n%w", errorEmptyInput)
+		return "", fmt.Errorf("Nothing to calculate. %w", errorEmptyInput)
 	}
 
-	for _, ch := range input {
-		if !(unicode.IsDigit(ch) || unicode.IsSpace(ch) || ch == rune('+') || ch == rune('-')) {
-			return "", fmt.Errorf("Invalid input.\n%w", errorInvalidInput)
-		}
-	}
-
-	re := regexp.MustCompile(`-?\d+`)
+	re := regexp.MustCompile(`[+-]?\d+[^+-]`)
 	sl := re.FindAllString(input, -1)
 
 	if len(sl) != 2 {
-		return "", fmt.Errorf("Number of operands must be equal two.\n%w", errorNotTwoOperands)
+		return "", fmt.Errorf("Number of operands must be equal two. %w", errorNotTwoOperands)
 	}
 
 	firstOperand, err := strconv.Atoi(strings.TrimSpace(sl[0]))
 	if err != nil {
-		return "", fmt.Errorf("First operand is not valid.\n%w", err)
+		return "", fmt.Errorf("First operand is not valid. %w", err)
 	}
 
 	secondOperand, err := strconv.Atoi(strings.TrimSpace(sl[1]))
 	if err != nil {
-		return "", fmt.Errorf("Second operand is not valid.\n%w", err)
+		return "", fmt.Errorf("Second operand is not valid. %w", err)
 	}
 
 	return strconv.Itoa(firstOperand + secondOperand), nil
